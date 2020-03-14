cab75976d40bb4b8ff504d3a391eb65ab8797cd7
Naim Malek <naimmalek7600@gmail.com>
2020-03-14 15:06:49 +0530

    1.0.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+// declare var $: any;
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+})
+export class AppComponent {
+	title = 'naimmalek';
+	loadAPI: any;
+	
+	constructor() {
+		
+	}
+
+
+
+}
